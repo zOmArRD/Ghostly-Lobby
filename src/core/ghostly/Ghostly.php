504b0132853012dce594c15cb9c724c7c878706c
zOmArRD <omarsena25@gmail.com>
2021-11-25 08:52:09 -0800

    Initial commit

---
+++ src/core/ghostly/Ghostly.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Created by PhpStorm.
+ *
+ * User: zOmArRD
+ * Date: 25/11/2021
+ *
+ * Copyright © 2021 Ghostly Network - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace core\ghostly;
+
+use pocketmine\plugin\PluginBase;
+
+class Ghostly extends PluginBase
+{
+
+}
